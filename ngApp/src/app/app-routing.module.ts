c02683e7b512bd686e3343f25db61ae03d959ed1
Amine Akrichi <122601450+amine-akrichi@users.noreply.github.com>
2023-03-27 08:05:02 +0100

    added profile description page

--- ngApp/src/app/app-routing.module.ts
+++ ngApp/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './shared/login/login.component';
 import { SignupComponent } from './shared/signup/signup.component';
 import { UserHomepageComponent } from './User/user-homepage/user-homepage.component';
+import { UserProfileComponent } from './User/user-profile/user-profile.component';
 import { UserSidebarComponent } from './User/user-sidebar/user-sidebar.component';
 
 const routes: Routes = [
@@ -18,6 +19,10 @@ const routes: Routes = [
       {
         path:'Homepage',
         component:UserHomepageComponent
+      },
+      {
+        path:'Profile',
+        component:UserProfileComponent
       }
     ]
   },
